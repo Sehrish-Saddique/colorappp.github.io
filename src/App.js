84b0c4d452eb803a9097d3a7ed7eb8bbd3d8e0d0
sehr <sehr.sehr1112@gmail.com>
2023-07-16 23:18:06 +0500

    Added files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+ import './App.css';
+import {Switch,Route} from 'react-router-dom';
+import Navbar from './components/navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+//import Signin from './components/Signin';
+import Login from './components/login';
+
+function App() {
+  return (
+    <div className="App">
+       <Switch>
+        <Route exact path="/" component={Navbar}/>
+        <Route exact path="/Home" component={Home}/>
+        <Route exact path="/About" component={About}/>
+        <Route exact path="/Contact" component={Contact}/>
+        <Route exact path="/Login" component={Login}/>
+       </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,38 @@
+ import React from 'react'
+ import {Link} from 'react-router-dom'
+ import myLogo from '../images/color-picker.png';
+
+ export default function Navbar() {
+   return (
+     <div> 
+     
+<nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+        <Link to="/Home" className="flex items-center">
+            <img src={myLogo} className="h-9 mr-2" alt="Color picker Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Color Picker</span>
+        </Link>
+        <div className="flex items-center">
+            <ul className="flex flex-row font-medium mt-0 mr-6 space-x-8 text-sm">
+                <li>
+                    <Link to="/Home" className="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</Link>
+                </li>
+                <li>
+                    <Link to="/About" className="text-gray-900 dark:text-white hover:underline">About Us</Link>
+                </li>
+                <li>
+                    <Link to="/Contact" className="text-gray-900 dark:text-white hover:underline">Contact</Link>
+                </li>
+                <li>
+                    <Link to="/Login" className="text-gray-900 dark:text-white hover:underline">Sign in</Link>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+ 
+ 
+     </div>
+   )
+ }
+ 
